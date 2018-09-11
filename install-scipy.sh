55fdafb39fec904fe248b72708c06f5690ba03bc
alex zeltov <azeltov@gmail.com>
2018-09-11 11:31:29 -0400

    Update install-scipy.sh

--- install-scipy.sh
+++ install-scipy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-/usr/bin/anaconda/envs/py35/bin/pip install -U scipy
+/usr/bin/anaconda/envs/py35/bin/pip uninstall numpy scipy -y
+/usr/bin/anaconda/envs/py35/bin/pip uninstall scikit-learn -y
+/usr/bin/anaconda/envs/py35/bin/conda install numpy scipy scikit-learn -y
