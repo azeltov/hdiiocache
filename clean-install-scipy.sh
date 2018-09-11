3dfad1bc2c0587deff35f4017aa9d11f938eb28e
alex zeltov <azeltov@gmail.com>
2018-09-11 11:56:02 -0400

    Delete clean-install-scipy.sh

--- clean-install-scipy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/anaconda/envs/py35/bin/pip uninstall numpy scipy -y
-/usr/bin/anaconda/envs/py35/bin/pip uninstall scikit-learn -y
-/usr/bin/anaconda/envs/py35/bin/conda install numpy scipy scikit-learn -y
